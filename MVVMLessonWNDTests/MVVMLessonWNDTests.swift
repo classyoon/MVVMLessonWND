0087d57166f2bf442d1ed2d98d2a15c9322c009f
classyoon <96508736+classyoon@users.noreply.github.com>
2025-03-17 18:20:28 -0400

    Initial Commit

---
+++ MVVMLessonWNDTests/MVVMLessonWNDTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMLessonWNDTests.swift
+//  MVVMLessonWNDTests
+//
+//  Created by Conner Yoon on 3/17/25.
+//
+
+import Testing
+@testable import MVVMLessonWND
+
+struct MVVMLessonWNDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
